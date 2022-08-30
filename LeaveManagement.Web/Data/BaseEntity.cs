77965d7fde6a93c61122e705768af3c8131b81cc
sudharsan.dev <sudharsan.dev@outlook.com>
2022-08-30 19:54:35 +0530

    Added database tables and migrations

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+    }
+}
